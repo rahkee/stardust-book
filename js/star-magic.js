f3ca3cf57a85f27b901e4ff449dc9ce9d57d69d2
Ray Villaraza <ray.villaraza@strongmind.com>
2023-10-23 19:20:51 -0700

    button activate and link to shopify

--- js/star-magic.js
+++ js/star-magic.js
@@ -1,3 +1,5 @@
+const mainContainer = document.querySelector('#naledi');
+
 window.addEventListener('wheel', (e) => {
     if (e.deltaY < 0) {
         mainContainer.classList.remove('journey-begins');
@@ -6,9 +8,7 @@ window.addEventListener('wheel', (e) => {
     }
 });
 
-const mainContainer = document.querySelector('#naledi');
-
 const homeCallToAction = document.querySelector('#begin-the-journey');
 homeCallToAction.addEventListener('click', () => {
-    mainContainer.classList.add('journey-begins');
+    mainContainer.classList.toggle('journey-begins');
 });
