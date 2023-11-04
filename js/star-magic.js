d9a71068fe5054fab722ff5fda02a59bce24adeb
Ray Villaraza <ray.villaraza@strongmind.com>
2023-11-04 12:16:23 -0700

    full parallax

--- js/star-magic.js
+++ js/star-magic.js
@@ -1,14 +1,7 @@
 const mainContainer = document.querySelector('#naledi');
 
-window.addEventListener('wheel', (e) => {
-    if (e.deltaY < 0) {
-        mainContainer.classList.remove('journey-begins');
-    } else {
-        mainContainer.classList.add('journey-begins');
-    }
-});
-
-const homeCallToAction = document.querySelector('#begin-the-journey');
-homeCallToAction.addEventListener('click', () => {
-    mainContainer.classList.toggle('journey-begins');
+window.addEventListener('load', (e) => {
+    setTimeout(function () {
+        mainContainer.classList.toggle('journey-begins');
+    }, 1000);
 });
