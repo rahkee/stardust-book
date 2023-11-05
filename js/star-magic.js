92a2bc50288a256e0dd81e5fc28c2a1da57a917b
Ray Villaraza <ray.villaraza@strongmind.com>
2023-11-05 12:27:12 -0700

    star adjustments

--- js/star-magic.js
+++ js/star-magic.js
@@ -1,6 +1,9 @@
 const mainContainer = document.querySelector('#naledi');
+const overlay = document.querySelector('.overlay');
 
 window.addEventListener('load', (e) => {
+    overlay.classList.add('hide');
+
     setTimeout(function () {
         mainContainer.classList.toggle('journey-begins');
     }, 1000);
